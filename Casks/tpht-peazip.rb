eb1ee657524a6e56499082554a17a54be5197162
Tristan Pavard <22837800+tpavard@users.noreply.github.com>
2025-05-21 13:03:05 +0200

    Update tpht-peazip.rb
    update PeaZip to v10.4.0

--- Casks/tpht-peazip.rb
+++ Casks/tpht-peazip.rb
@@ -2,10 +2,10 @@ cask "tpht-peazip" do
   arch arm: "aarch64",
        intel: "x86_64"
 
-  version "10.2.0"
+  version "10.4.0"
 
-  sha256 arm: "79a1947f88cf98c958f508247fa38ebf2e0b08a4a42e653f4aad465f63c5c82d",
-         intel: "66835755439a952631f6b3271d31a264402e0d77245f9a3fd21a118be2d8d3ac"
+  sha256 arm: "546686c6f592ffa65bcedd4b47e76487e9ec980301ca4d4f0d9cae02929ee6e0",
+         intel: "0a7058c9cabc4be43809a55092c3f1f5fc3bc4bc2a3e7c275ed21a25a0c6d556"
 
   url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
     verified: "github.com/peazip/PeaZip/"
