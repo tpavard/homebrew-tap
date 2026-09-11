f8fbbae73c323237403c234cbfea126349b5d31e
Andrew <andrew@drewbitt.com>
2026-09-11 11:59:41 -0400

    tpht-peazip: drop deprecated verified parameter in url stanza
    Homebrew odepreates the verified parameter in the url stanza; it is
    accepted as a no-op. The default URL verification behaviour covers this
    URL already.

--- Casks/tpht-peazip.rb
+++ Casks/tpht-peazip.rb
@@ -7,8 +7,7 @@ cask "tpht-peazip" do
   sha256 arm: "c94a5678b6bbe7c69ae5dba2193d2879efdeeef8a82900edd9507884c4f1be7a",
          intel: "812e9392e9a2da71969ba356956d6fd4936fa546de73912351b6e64cc477ac54"
 
-  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
-    verified: "github.com/peazip/PeaZip/"
+  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg"
 
   name "PeaZip"
   desc "Free Zip / Unzip software and Rar file extractor."
